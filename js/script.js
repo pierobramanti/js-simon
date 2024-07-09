733dca770a4b1ee13ebfcb8be38b74504fa373e4
pierobramanti <piero.bramanti94@gmail.com>
2024-07-09 16:55:58 +0200

    [UPD] created function to generate random numbers

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-// Creo un'array vuoto //
-
 const numbers = [];
 
-// Creo un array vuoto dove inserire i numeri dell'utente //
-
+// Creo un array vuoto dove inserire i numeri dell'utente
 const userNumbers = [];
+
+console.log(numbers);
+
+// Creo una funzione per generare i cinque numeri casuali
+function numberGenerator() {
+    while (numbers.length < 5) {
+        let numb = Math.floor(Math.random() * 100) + 1;
+
+        if (!numbers.includes(numb)) {
+            numbers.push(numb);
+        }
+    }
+}
+
+// Genero i numeri casuali
+numberGenerator();
+
+// Stampo l'array di numeri generati
+console.log(numbers);
