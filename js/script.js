05c8f2984345330d55199293e3d685925a0d51e3
pierobramanti <piero.bramanti94@gmail.com>
2024-07-09 17:11:15 +0200

    [UPD] printed numbers in html

--- js/script.js
+++ js/script.js
@@ -5,6 +5,18 @@ const userNumbers = [];
 
 console.log(numbers);
 
+// Genero i numeri casuali
+numberGenerator();
+
+// Stampo l'array di numeri generati
+console.log(numbers);
+
+const numbersElement = document.getElementById('numbers');
+
+// Aggiorno il contenuto dell'elemento <p> con i numeri generati
+numbersElement.innerHTML = numbers
+
+
 // Creo una funzione per generare i cinque numeri casuali
 function numberGenerator() {
     while (numbers.length < 5) {
@@ -16,8 +28,4 @@ function numberGenerator() {
     }
 }
 
-// Genero i numeri casuali
-numberGenerator();
 
-// Stampo l'array di numeri generati
-console.log(numbers);
